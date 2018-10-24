84b3ef0d610ea805ba34d40c2c1fc7fb86bd4bdf
evsantos <evsantos@EVSANTOS-PC.br.oracle.com>
2018-10-24 19:03:34 -0300

    Refactory

---
+++ refactory/gulpfile.js
@@ -0,0 +1,68 @@
+const gulp = require('gulp')
+    , del = require('del')
+    , path = require('path')
+    , less = require('gulp-less')
+    , sourcemaps = require('gulp-sourcemaps')
+    , minifyCss = require('gulp-clean-css');
+
+const PATH = {
+    input: 'styles/**/*.less',
+    output: 'build'
+}
+
+gulp.task('delete', () => del(PATH.output) );
+
+gulp.task('styles', () => {
+    gulp.src(PATH.input)
+    .pipe( 
+        less( {
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }).on('error', (erro) => {
+            console.log('LESS, erro compilação: ' + erro.filename);
+            console.log(erro.message);
+        })
+    )
+    .pipe( 
+        minifyCss({
+            compatibility: 'ie8',
+            level: 1
+        }) 
+    )
+    .pipe( gulp.dest(PATH.output) )
+});
+
+gulp.task('map', () => {
+    gulp.src(PATH.input)
+    .pipe(sourcemaps.init())
+    .pipe( 
+        less( {
+            paths: [ path.join(__dirname, 'less', 'includes') ]
+        }).on('error', (erro) => {
+            console.log('LESS, erro compilação: ' + erro.filename);
+            console.log(erro.message);
+        })
+    )
+    .pipe(sourcemaps.write('maps'))
+    .pipe( gulp.dest(PATH.output) )
+});
+
+gulp.task( 'default', () => {
+    gulp.watch( PATH.input, ['map'] );
+});
+
+gulp.task('build', ['delete'], () => {
+    gulp.start('styles');
+})
+
+
+// evandro, tudo no css que tiver:
+
+// ../oi_imagens/{arquivo}
+// ../oi_css/{arquivo}
+// ../oi_pos_imagens/{arquivo}
+
+// tem que trocar para
+
+// ../../image/oi_imagens/{arquivo}
+// ../../image/oi_css/{arquivo
+// ../../image/oi_pos_imagens/{arquivo}
---
+++ refactory/scripts/oi_pos_appMinhaOi.js
@@ -0,0 +1,80 @@
+define("oi_pos_appMinhaOi", [], function () {
+
+    try {
+        var $ = jQuery;
+
+        $(document).ready(function () {
+
+            var transactionTimer = 5000;
+
+            var tabChange = function () {
+                var tabs = $("#appMinhaOi-container .nav > li:visible");
+                var active = tabs.find(".nav-link").filter(".active");
+                var nextTab = active.parent("li").next("li").find(".nav-link");
+                var next = nextTab.length ? nextTab : tabs.filter(":first-child").find(".nav-link");
+                next[0].click();
+            }
+
+            var canPlay = false;
+
+            var videoTags = $("#appMinhaOi-TabContent .tab-pane video");
+
+            if (videoTags[0].canPlayType && videoTags[0].canPlayType('video/mp4').replace(/no/, '')) {
+                canPlay = true;
+            }
+
+            if (canPlay) {
+
+                $.each(videoTags, function () {
+                    $(this)[0].addEventListener("ended", function () {
+                        tabChange();
+                    });
+                });
+
+                videoTags[0].play();
+
+                $("#appMinhaOi-container .nav-link").click(function (e) {
+
+                    e.preventDefault();
+
+                    if (timeout) {
+                        clearTimeout(timeout);
+                    }
+
+                    timeout = setTimeout(function () {
+                        var videoTag = $("#appMinhaOi-TabContent .tab-pane.active video");
+                        videoTag[0].play();
+                    }, 1000);
+
+                });
+
+            } else {
+
+                var tabCycle = setInterval(tabChange, transactionTimer)
+                var timeout;
+
+                $("#appMinhaOi-container .nav-link").click(function (e) {
+
+                    e.preventDefault();
+
+                    clearInterval(tabCycle);
+
+                    if (timeout) {
+                        clearTimeout(timeout);
+                    }
+
+                    timeout = setTimeout(function () {
+                        tabChange();
+                        tabCycle = setInterval(tabChange, transactionTimer);
+                    }, transactionTimer);
+
+                });
+            }
+
+        });
+
+    } catch (err) {
+        console.error(err);
+    }
+
+});
